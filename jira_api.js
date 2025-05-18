48d0669a49655b97aaee5b3cd88e8368cc332b7b
Roman Bauer <r.bauer@vk.team>
2025-05-18 16:01:59 +0300

    main: proxy for jira

--- jira_api.js
+++ jira_api.js
@@ -4,3 +4,10 @@ const fetch = require("node-fetch");
 // curl -H "Authorization: Bearer [KEY]"issue/KEY-1234
 // https://developer.atlassian.com/server/jira/platform/jira-rest-api-examples/
 // https://jira.vk.team/rest/api/2/user?username=r.bauer@mail.msk&expand=groups&_=1680986676887
+
+// curl -H 'Authorization: Bearer [KEY]' https://jira.vk.team/rest/api/2/user\?username\=r.bauer@mail.msk\&expand\=groups\&_\=1680986676887
+fetch('https://jira.vk.team/rest/api/2/user\?username\=r.bauer@mail.msk\&expand\=groups\&_\=1680986676887', {
+    headers: {
+        'Authorization': 'Bearer [KEY]',
+    },
+});
---
+++ proxy.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fetch = require('node-fetch');
+const url = require('url');
+require("dotenv").config();
+
+const { PROXY_PORT } = process.env;
+
+// curl -H 'X-Host: https://qwerty.ru' -H 'Authorization: Bearer 1234567' localhost:3001/foo/bar
+// curl -H 'Authorization: Bearer x1234567' localhost:3001/foo/bar?x-host=https://qwerty.ru&x-authorization=Bearer%20x1234567
+
+const webServer = http.createServer(async (req, res) => {
+    const query = url.parse(req.url, true).query;
+
+    console.log('req:', req.url, query);
+
+    const host = req.headers['x-host'] || query['x-host'];
+    const authorization = req.headers['authorization'] || query['x-authorization'];
+    const cb = req.headers['x-callback'] || query['x-callback'];
+
+    delete query['x-host'];
+    delete query['x-authorization'];
+    delete query['x-callback'];
+
+    if (host) {
+        const resp = await fetch(host + req.url, {
+            headers: {
+                'Authorization': authorization,
+            },
+        });
+
+        const raw = await resp.text();
+
+        if (cb) {
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.end(`window['${cb}']('${raw}')`);
+
+            return;
+        }
+
+        res.end(raw);
+
+        return;
+    }
+
+    res.end('empty x-host');
+});
+
+webServer.listen(parseInt(PROXY_PORT), 'localhost');
